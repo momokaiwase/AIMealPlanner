8ba49c1b668cfcfbaa24efd9e1f49e127a871cc8
sonan2 <sonan@bc.edu>
2024-11-06 20:06:59 -0500

    update index.js

--- mealplanner/src/index.js
+++ mealplanner/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Routes>
+      <Routes basename="/MealPlanner">
         <Route path="/" element={<Select />} />
         <Route path="/plan" element={<Plan />} />
         <Route path="/loading" element={<Loading />} />
